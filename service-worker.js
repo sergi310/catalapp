d31b4cc6d9fc29731caa1aa6840b2632c3f76cee
sergi310 <sergibertran8@gmail.com>
2025-05-12 11:29:38 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/styles.css',
+        '/script.js',
+        // Aquí pots afegir altres fitxers que vols emmagatzemar
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
